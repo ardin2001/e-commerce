7644e3198250450f1c5069050c58bece62b65b47
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2024-05-27 21:29:10 +0700

    initial project

---
+++ src/app/api/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server"
+export async function GET(req :NextRequest) {   
+    return NextResponse.json({ 
+        status : true,
+        statusCode : 200,
+        message : "success",
+        data : {
+            text : "Hello World!"
+        }
+     })
+  }
